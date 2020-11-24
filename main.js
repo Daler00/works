42414a69b0dddd3be9f5d6c4c0586f2a32f70270
Daler <72223316+Daler00@users.noreply.github.com>
2020-11-24 20:51:03 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+function adding(number1,number2){
+    return number1+number2
+}
+document.getElementById("h1").innerHTML=adding(4,7)
+
+function convert(number){
+    return number*60
+}
+document.getElementById("h1").innerHTML=convert(7)
+
+function calcAge(number){
+    return number*365
+}
+document.getElementById("h1").innerHTML=calcAge(70)
+
+function additional(number){
+    return number+1
+}
+document.getElementById("h1").innerHTML=additional(-4)
+
+function triArea(base,height){
+    return base*height/2
+}
+document.getElementById("h1").innerHTML=triArea(4,7)
+
+function triArea(base,height){
+    return base*height/2
+}
+document.getElementById("h1").innerHTML=triArea(7,11)
